86d413c68dac5b9fa482a20d4ec08fb8d0853038
WilliamZang <chengwei.zang.1985@gmail.com>
2015-03-22 00:27:29 +0800

    Initial Commit

---
+++ BoardGamesHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BoardGamesHelper
+//
+//  Created by ZangChengwei on 15/3/22.
+//  Copyright (c) 2015年 ZangChengwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BoardGamesHelper/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BoardGamesHelper
+//
+//  Created by ZangChengwei on 15/3/22.
+//  Copyright (c) 2015年 ZangChengwei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
